19efdcc15869fd24006c898db2804847c713a25a
Clare <chblackburne@gmail.com>
2020-04-12 10:52:23 +0100

    programmatically create pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import { rhythm } from "../utils/typography"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -8,7 +9,17 @@ import SEO from "../components/seo"
 const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <h1>{JSON.stringify(data)}</h1>
+    <h4>{data.allMarkdownRemark.totalCount} Producers</h4>
+    {data.allMarkdownRemark.edges.map(({ node }) => (
+      <div key={node.id}>
+        <Link to={node.fields.slug}>
+          <h3>
+            {node.frontmatter.title} <span>— {node.frontmatter.date}</span>
+          </h3>
+          <p>{node.excerpt}</p>
+        </Link>
+      </div>
+    ))}
   </Layout>
 )
 
@@ -23,6 +34,9 @@ export const query = graphql`
           frontmatter {
             title
             date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
           }
           excerpt
         }

218f4a3e801dcfdb084550dfa92b9a025219057d
Clare <chblackburne@gmail.com>
2020-04-12 10:26:08 +0100

    add typography

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Supporting Settle`,
+    description: `Local Producers near Settle, North Yorkshire.`,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -15,6 +15,12 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {

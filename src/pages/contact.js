a537a3e52894051b354f2a9559eb841e5b2c8e15
Clare <chblackburne@gmail.com>
2020-04-13 14:40:33 +0100

    update contact form and content

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,34 +1,13 @@
 import React from "react"
-import { Link } from "gatsby"
 import SEO from "../components/seo"
+import Heading from "../components/heading"
+import ContactForm from "../components/contact-form"
 
 const Contact = () => (
   <>
     <SEO title="Contact" />
-
-    <form method="post" netlify-honeypot="bot-field" data-netlify="true">
-      <input type="hidden" name="bot-field" />
-
-      <label>
-        Name
-        <input type="text" name="name" id="name" />
-      </label>
-      <label>
-        Email
-        <input type="email" name="email" id="email" />
-      </label>
-      <label>
-        Subject
-        <input type="text" name="subject" id="subject" />
-      </label>
-      <label>
-        Message
-        <textarea name="message" id="message" rows="5" />
-      </label>
-      <button type="submit">Send</button>
-      <input type="reset" value="Clear" />
-    </form>
-    <Link to="/">Go back to the homepage</Link>
+    <Heading heading="Contact Form" />
+    <ContactForm />
   </>
 )
 

2cbe5996267027cf1e21d7040d0f4c9b16cedebd
Clare <chblackburne@gmail.com>
2020-04-15 20:31:35 +0100

    remove

---
+++ src/data/positivestuff.js
@@ -0,0 +1,122 @@
+export const positiveStuff = [
+  {
+    title: "Wild Inside Newsletter - John Muir Trust",
+    website: "https://mailchi.mp/johnmuirtrust.org/wildinside",
+    image: "",
+    description:
+      "Free weekly newsletter bringing the outdoors indoors! Includes things to read, watch, & do, all related to nature.",
+  },
+  {
+    title: "John Krasinski's Some Good News",
+    website: "https://www.youtube.com/channel/UCOe_y6KKvS3PdIfb9q9pGug",
+    image: "",
+    description:
+      "YouTube series by Office actor John Krasinski, bringing you heart-warming good news stories from around the world. Just 20 mins long and reliably feelgood!",
+  },
+  {
+    title: "WeRateDogs",
+    website: "https://twitter.com/dog_rates",
+    image: "",
+    description:
+      "Don't worry if you're not on Twitter - you can still see the page! Daily photos and videos of cute dogs.",
+  },
+  {
+    title: "Victoria Hall's List of Silver Linings",
+    website: "https://www.settlevictoriahall.org.uk/silver-linings",
+    image: "",
+    description: "Fantastic list of online culture and entertainment",
+  },
+  {
+    title: "Ramblers #RoamSweetHome",
+    website: "https://www.ramblers.org.uk/go-walking/roam-sweet-home.aspx",
+    image: "",
+    description:
+      "Sign up to a challenge to keep your step count up. Communities on Twitter, Facebook, & Instagram",
+  },
+  {
+    title: "National Theatre at Home",
+    website: "https://www.nationaltheatre.org.uk/",
+    image: "",
+    description: "Word-class theatre, from the comfort of your sofa.",
+  },
+  {
+    title: "Aquarium Live Webcams",
+    website: "https://www.montereybayaquarium.org/animals/live-cams",
+    image: "",
+    description:
+      "Lots of zoos and parks are streaming live coverage. This one by Monterey Bay Aquarium is particularly good.",
+  },
+  {
+    title: "Hope in a Time of Crisis",
+    website: "https://www.theguardian.com/news/series/hope-in-a-time-of-crisis",
+    image: "",
+    description: "The Guardian's daily roundup of good new stories.",
+  },
+  {
+    title: "Podcast: How to Fail with Elizabeth Day",
+    website: "https://howtofail.podbean.com/",
+    image: "",
+    description:
+      "Fantastic podcast with recent episodes focusing on how to cope with anxiety and stress around Corona Virus.",
+  },
+  {
+    title: "Google Arts & Culture",
+    website: "https://artsandculture.google.com/partner",
+    image: "",
+    description:
+      "2500 museums around the world have opened up their collections online for you to discover!",
+  },
+  {
+    title: "Creative Boom Advice",
+    website:
+      "https://www.creativeboom.com/tips/keeping-the-spirits-up-during-lockdown-how-to-be-happy-in-isolation",
+    image: "",
+    description:
+      "Creative Boom is a UK-based online magazine and podcast, dedicated to artists, designers, illustrators, photographers and anyone working in visual arts and communications. ",
+  },
+  {
+    title: "Cheryl Strayed talks to Margaret Atwood",
+    website:
+      "https://www.nytimes.com/2020/04/08/podcasts/sugar-calling-margaret-atwood-coronavirus.html",
+    image: "",
+    description:
+      "Free New York Times podcast between writers Cheryl Strayed and Margaret Atwood - 'about hard times, pansies and eight decades of getting it done'.",
+  },
+  {
+    title: "Yoga with Adrienne",
+    website: "https://www.youtube.com/user/yogawithadriene",
+    image: "",
+    description:
+      "There are loads of free online exercise videos on YouTube, but Adrienne's yoga classes are reliably uplifting - in part due to guest appearances from her dog Benji!",
+  },
+  {
+    title: "PE with Joe",
+    website:
+      "https://www.youtube.com/watch?v=Rz0go1pTda8&list=PLyCLoPd4VxBvQafyve889qVcPxYEjdSTl",
+    image: "",
+    description:
+      "Everyone has heard of this one! Joe Wicks keeping the nation fit on YouTube.",
+  },
+  {
+    title: "Walter Presents - free boxsets",
+    website: "https://www.channel4.com/collection/walter-presents",
+    image: "",
+    description:
+      "Large selection of award-winning foreign-language drama - perfect to catch up with the latest Scandi Noir even if it's sunny outside...",
+  },
+  {
+    title: "National Parks Online Experience",
+    website:
+      "https://artsandculture.withgoogle.com/en-us/national-parks-service",
+    image: "",
+    description:
+      "Explore the national parks of America through your laptop - best experienced wearing headphones. Great escapism!",
+  },
+  {
+    title: "Storytime from Space",
+    website: "https://storytimefromspace.com/library/",
+    image: "",
+    description:
+      "One for the kids! Astronauts in the International Space Station reading stories from popular childrens books.",
+  },
+]

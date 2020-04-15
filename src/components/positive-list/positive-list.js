5ad181902de2814eeb8edd86e5a5b4be1379e088
Clare <chblackburne@gmail.com>
2020-04-15 12:23:40 +0100

    feat: positive stuff

--- src/components/positive-list/positive-list.js
+++ src/components/positive-list/positive-list.js
@@ -1,21 +1,24 @@
 import React from "react"
 import styles from "./positive-list.module.css"
-
+import { positiveStuff } from "../../data/positivestuff"
 const PositiveList = () => {
   return (
     <div className={styles.positiveList} id="positivity">
-      <p>Under construction!</p>
-      <p>
-        In the meantime, check out the fantastic list of online entertainment on
-        the{" "}
-        <a
-          href="https://www.settlevictoriahall.org.uk/silver-linings"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Victoria Hall website
-        </a>
-      </p>
+      <p>I'll try and update this regularly, so keep checking back for more!</p>
+      <ul className={styles.list}>
+        {positiveStuff.map(item => {
+          const { title, description, website, image } = item
+          return (
+            <li key={title} className={styles.item}>
+              <h2>{title}</h2>
+              <a href={website} target="_blank" rel="noopenner noreferrer">
+                {website}
+              </a>
+              <p className={styles.description}>{description}</p>
+            </li>
+          )
+        })}
+      </ul>
     </div>
   )
 }
